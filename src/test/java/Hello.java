c2ad4e1ec8f7efbac8ae746ee73728b736da9fe1
Melike24 <72043988+Melike24@users.noreply.github.com>
2020-11-02 03:14:56 +0100

    Added changes to hello from github

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello awseome");
+        System.out.println("Hello frome github some changes awseome");
     }
 }
